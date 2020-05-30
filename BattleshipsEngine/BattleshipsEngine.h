2fd2f0812975e801cba385de61d5619094849589
Andrey Volobuev <blob8129@gmail.com>
2020-05-30 23:55:18 +1000

    inital commit

---
+++ BattleshipsEngine/BattleshipsEngine.h
@@ -0,0 +1,19 @@
+//
+//  BattleshipsEngine.h
+//  BattleshipsEngine
+//
+//  Created by Andrey Volobuev on 30/05/2020.
+//  Copyright © 2020 Andrey Volobuev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BattleshipsEngine.
+FOUNDATION_EXPORT double BattleshipsEngineVersionNumber;
+
+//! Project version string for BattleshipsEngine.
+FOUNDATION_EXPORT const unsigned char BattleshipsEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BattleshipsEngine/PublicHeader.h>
+
+
